485f0c56e096a9bac0085c58f2afaca972bf4488
Steven Luu <Stevenluuchs@gmail.com>
2023-11-08 15:39:39 -0600

    Added a NavigationView with Vstack and picker

--- EmojiSwitcher2/ContentView.swift
+++ EmojiSwitcher2/ContentView.swift
@@ -7,15 +7,29 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable {
+    case 🚎, 🚌, 🥇, 🥘
+}
+
 struct ContentView: View {
+   @State var selection: Emoji = .🥘
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 250))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach( Emoji.allCases, id: \.self) {emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Picker")
+            .padding()
         }
-        .padding()
     }
 }
 
