af40382c4e64bed853aedde8d7a33a142fe7bc8f
Steven Luu <Stevenluuchs@gmail.com>
2023-11-08 13:47:18 -0600

    Initial Commit

---
+++ EmojiSwitcher2/EmojiSwitcher2App.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiSwitcher2App.swift
+//  EmojiSwitcher2
+//
+//  Created by Steven Luu on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiSwitcher2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
